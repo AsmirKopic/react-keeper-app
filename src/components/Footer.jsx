c8a698b600aa729ee317d0acbf58c97e08147c98
unknown <asmir.kopic123@gmail.com>
2022-06-29 23:48:16 +0200

    components added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const year = new Date()
+const currentYear = year.getFullYear()
+
+export default function Footer() {
+  return (
+    <footer>
+    <p>Copyright Asmir Kopić © {currentYear}</p>
+
+    </footer>
+
+
+    
+  )
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Note() {
+  return (
+    <div className='note'>
+        <h1>Note title</h1>
+        <p>Lorem lipsum neki text</p>
+    
+    </div>
+  )
+}
