bc86fa474ab4679ac44ed1e3e3b9f8701e603eb6
unknown <asmir.kopic123@gmail.com>
2022-06-30 22:33:32 +0200

    added map function to render notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import notes from '../notes'
 
-export default function Note() {
+export default function Note(props) {
   return (
     <div className='note'>
-        <h1>Note title</h1>
-        <p>Lorem lipsum neki text</p>
-    
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
   )
 }
---
+++ src/notes.jsx
@@ -0,0 +1,49 @@
+const notes = [
+    {
+        key: 1,
+        title: "Delegation",
+        content: "Q. How many programmers does it take to change linght bulb?"
+    },
+    {
+        key: 2,
+        title: "Loops",
+        content: "Some smart text about loops.."
+    },
+    {
+        key: 3,
+        title: "Array",
+        content: "Q. Did we learn some new about React arrays?"
+    },
+    {
+        key: 4,
+        title: "Another title",
+        content: "Just another text about something"
+    },
+    {
+        key: 5,
+        title: "Lorem ipsum text",
+        content: "Some cool text about keeper application that i just build.."
+    },
+    {
+        key: 6,
+        title: "Array",
+        content: "Q. Did we learn some new about React arrays?"
+    },
+    {
+        key: 7,
+        title: "Another title",
+        content: "Just another text about something"
+    },
+    {
+        key: 8,
+        title: "Lorem ipsum text",
+        content: "Some cool text about keeper application that i just build.."
+    },
+    {
+        key: 9,
+        title: "Lorem ipsum text",
+        content: "Some cool text about keeper application that i just build.."
+    }
+]
+
+export default notes;
