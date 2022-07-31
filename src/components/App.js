ded36d0102fe9881c470d539346a8de0b459eb29
unknown <asmir.kopic123@gmail.com>
2022-07-31 23:28:14 +0200

    complete crud note operations and added styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,34 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
 import InputArea from "./InputArea";
 
 function App() {
 
+  const [notes, setNotes] = useState([])
+
+  function addNote(note) {
+    setNotes(prevNotes => {
+      return [...prevNotes, note]
+    });
+  }
+
+  function deleteNote(id) {
+
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      }); 
+    });
+  }
+
   return (
     <div>
       <Header />
-      <InputArea />
-
-      {notes.map(note => (
-        <Note key={note.key} title={note.title} content={note.content} />
+      <InputArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote} />
       ))}
-
       <Footer />
 
     </div>
--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,16 +1,42 @@
 import React, { useState } from 'react'
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
 
-export default function InputArea() {
+export default function InputArea(props) {
+
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote, 
+                [name]: value
+            };
+        });
+    }
+
+    function submitNote(event){
+        event.preventDefault();
+        props.onAdd(note);
+
+        setNote({
+            title: "",
+            content:""
+        })
+    }
 
     return (
-
         <div>
-            <form>
-                <input name="title" placeholder='Title'/>
-                <textarea name='content' placeholder='Take a note..' rows={3} />
-                <button>+</button>
+            <form className="create-note">
+                <input onChange={handleChange} name="title" value={note.title} placeholder='Title'/>
+                <textarea onChange={handleChange} name='content' value={note.content} placeholder='Take a note..' rows={3} />
+                <Fab onClick={submitNote}><AddIcon /></Fab>
             </form>
-            
         </div>
     )
 }
